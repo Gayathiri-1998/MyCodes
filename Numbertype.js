04b12aea7cd86e7f4496e6ec97fa3324855f539e
Gayathiri-1998 <gayathirirs1998@gmail.com>
2026-02-02 20:42:38 +0530

    Add files via upload
    Numbertype Assignments

---
+++ Numbertype.js
@@ -0,0 +1,11 @@
+let number = -5;   // change this value to test
+
+if (number > 0) {
+  console.log("Positive");
+} 
+else if (number < 0) {
+  console.log("Negative");
+} 
+else {
+  console.log("Zero");
+}
